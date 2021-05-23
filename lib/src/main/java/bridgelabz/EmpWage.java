73fdddbca784ca6934291d5df0dd16e47140df40
kirantidke <kirantidke2013@.com>
2021-05-23 23:45:14 +0530

    file modified

--- lib/src/main/java/bridgelabz/EmpWage.java
+++ lib/src/main/java/bridgelabz/EmpWage.java
@@ -4,7 +4,14 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to employee wage");
-
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+				// checking the presence of Employee
+		if ( empCheck == IS_FULL_TIME )
+		{
+			System.out.println( " Employee is Present ");
+		}
+		else
+		System.out.println(" Employee is Absent ");
 	}
-
 }
