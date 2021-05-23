bb558294e92a89fc551632bcd58ade5ede9444c0
kirantidke <kirantidke2013@.com>
2021-05-24 00:05:44 +0530

    file modified

--- lib/src/main/java/bridgelabz/EmpWage.java
+++ lib/src/main/java/bridgelabz/EmpWage.java
@@ -1,25 +1,27 @@
 package bridgelabz;
-
-public class EmpWage {
-
-	public static void main(String[] args) {
-		System.out.println("welcome to employee wage");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int Wage_per_hour = 20;
+public class EmpWage 
+{
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int Wage_per_hour = 20;
+	public static void main(String[] args)
+	{
+		System.out.println(" Welcome To Emloyee Wage");
 		int Emp_wage_hour = 0;
-		double empCheck = Math.floor(Math.random()*10)%3;
-		 //  Part Time wagge and Full time wage
-		if ( empCheck == IS_FULL_TIME )
-			//Employee is Present
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
+			// used Switch case for Employe_Wage
+		switch (empCheck)
+		{
+			case IS_FULL_TIME:
 			Emp_wage_hour = 8 ;
-		else if ( empCheck == IS_PART_TIME )
-			//Employee is Absent
+			break;
+			case IS_PART_TIME:
 			Emp_wage_hour = 4;
-		else
+			break;
+			default:
 			Emp_wage_hour = 0;
-
-			int Emp = Emp_wage_hour * Wage_per_hour;
+		}
+		int Emp = Emp_wage_hour * Wage_per_hour;
 		System.out.println("Wage of a employee per day : " + Emp);
 	}
 }
